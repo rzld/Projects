5ac3810a0e3a970ce2178e88cfb0bb209c7ddb10
Rizaldi Tri Yanuar <rzldty@gmail.com>
2018-08-09 20:15:49 +0700

    Updated ARCore to 1.4.0

--- AR/World Capture/Assets/Scripts/WorldCapture.cs
+++ AR/World Capture/Assets/Scripts/WorldCapture.cs
@@ -19,7 +19,6 @@ public class WorldCapture : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//maybe?
 		//spawnPoint.position = Camera.main.transform.position;
 		if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began) {
 			Debug.Log("Touch");
@@ -28,26 +27,23 @@ public class WorldCapture : MonoBehaviour {
 	}
 
 	void CaptureWorld() {
-		//capture screen to texture
+		// capture screen to texture
 		//yield return new WaitForEndOfFrame();
 		planeTex = ScreenCapture.CaptureScreenshotAsTexture();
 		//yield return new WaitForEndOfFrame();
 
+		// get camera pose
 		Pose cameraPose = new Pose(Camera.main.transform.position, Camera.main.transform.rotation);
 
-		//create anchor
+		// create anchor a few points in front of camera
 		Pose planePosition = new Pose(cameraPose.position + cameraPose.forward * 1.5f, cameraPose.rotation);
 		anchor = Session.CreateAnchor(planePosition);
 		
-		//instantiate game object and put texture to quad
+		// instantiate game object, ancrhor the game object and put texture to quad
 		GameObject newPlane = Instantiate(plane, cameraPose.position + cameraPose.forward * 1.5f, cameraPose.rotation);
 		newPlane.transform.parent = anchor.transform;
 		//newPlane.transform.localScale = new Vector3(Screen.width, Screen.height, 1.0f);
 		newPlane.GetComponent<MeshRenderer>().material.mainTexture = planeTex;
 		planeTex.Apply();
-	}
-
-	public void LateUpdate() {
-	 	//StartCoroutine(CaptureWorld());
 	}
 }
